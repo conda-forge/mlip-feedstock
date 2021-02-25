104eaa1543943bcf12fe3875d755fd98aa34c0c5
Jan Janssen <jan-janssen@users.noreply.github.com>
2021-02-25 15:03:41 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,11 +9,9 @@ make mlp
 export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_rmaps_base_oversubscribe=yes
-if [[ -z "$MACOSX_DEPLOYMENT_TARGET" ]]; then
-  cd ../test
-  ../make/mlp self-test
-  cd ..
-fi
+cd ../test
+../make/mlp self-test
+cd ..
 
 # Install
 mkdir -p ${PREFIX}/bin
