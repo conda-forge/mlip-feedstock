bf128dd901fe21ede06825f4d9bc324fd072b854
Jinzhe Zeng <jinzhe.zeng@rutgers.edu>
2024-01-03 01:38:06 -0500

    set compilers

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,18 +2,19 @@
 
 if [[ "${mpi}" == "nompi" ]]; then
     CONFIGURE_ARGS="--no-mpi"
+    MAKE_ARGS="CC=${CC} CXX=${CXX} FC=${FC}"
 fi
 
 # Build 
 ./configure --blas=openblas --blas-root=${PREFIX} ${CONFIGURE_ARGS}
-make mlp
-make libinterface
+make mlp ${MAKE_ARGS}
+make libinterface ${MAKE_ARGS}
 
 # Test 
 export OMPI_MCA_plm=isolated
 export OMPI_MCA_btl_vader_single_copy_mechanism=none
 export OMPI_MCA_rmaps_base_oversubscribe=yes
-make test
+make test ${MAKE_ARGS}
 
 # Install
 mkdir -p ${PREFIX}/bin
